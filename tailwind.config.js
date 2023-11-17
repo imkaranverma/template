332976b43143535ab6ea08702ed1d77211af97d3
imkaranverma <106688614+imkaranverma@users.noreply.github.com>
2023-11-17 18:40:07 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter':'Inter',
+        'open-sans':'Open Sans',
+        'poppins':'Poppins'
+      },
+      colors: {
+        'green-blue':'#ADE5E5',
+        'blue-dark': '#12294B'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
